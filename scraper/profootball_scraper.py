4814f4d831d49401d6f8a9029a3e4625db2b1340
naivelogic <threetimberland@gmail.com>
2018-08-21 23:54:29 -0700

    Create profootball_scraper.py

---
+++ scraper/profootball_scraper.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+
+# Imports and Date Processing
+import feedparser
+import pandas as pd
+import re
+from datetime import datetime
+import feedparser
+
+# Create dataframe to collect NFL team team taxonomy from 
+# profootball talk site. Each team is organized by:
+# /team/[conference]/[team name]/feed/
