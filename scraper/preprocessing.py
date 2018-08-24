3de97b8e077bc982ec82840cf74296353f928752
naivelogic <threetimberland@gmail.com>
2018-08-23 23:32:35 -0700

    Create preprocessing.py
    Needs to work on data normalization (work in progress - may convert to another file)

---
+++ scraper/preprocessing.py
@@ -0,0 +1,61 @@
+# preprocessing.py
+
+"""
+Listing of functions to assist with formatting text including:
+- Defining the words to remove from the corpus and n-grams
+- Data cleaning
+- data normalization (work in progress - may convert to another file)
+"""
+
+
+import nltk
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+
+def remove_stop_words():
+    '''
+    Remove any stopwards as needed
+    '''
+    default_stopwords = set( stopwords.words('english') ).union( set(ENGLISH_STOP_WORDS) )
+    my_additional_stop_words = (nfl_news_stopwords) # Add any additional stopwords we dont want and update the list
+    default_stopwords = default_stopwords.union(my_additional_stop_words)
+    
+    return(default_stopwords)   
+
+
+
+import re
+
+def preprocess_post(post):
+    post = post.lower()
+    post = re.sub('\n', ' ', post)
+    post = re.sub(r'[^\w\s]','',post)
+    
+    return post
+
+"""
+listing of words that im currently considering not applicable
+for purposes of n-grams, they may be incorporated when we perform 
+sentiment analysis and start using LSTM
+"""
+nfl_news_stopwords = ['game',
+    'team',
+    'season',
+    'year',
+    'like',
+    'time',
+    'just',
+    'nfl',
+    '2018',
+    'august',
+    'field',
+    'games',
+    'who',
+    'years',
+    'pm',
+    'et',
+    'aug',
+    'say',
+    'lot',
+    'august','let','make','come','know'
+]
