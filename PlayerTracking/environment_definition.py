5eb31c140b3aeda05369fa87be25c33e2682070a
naivelogic <threetimberland@gmail.com>
2020-02-02 21:10:48 -0800

    Update environment_definition.py

--- PlayerTracking/environment_definition.py
+++ PlayerTracking/environment_definition.py
@@ -1,13 +1,39 @@
+#Workspace.from_config()
+import yaml
+
+# Initialize a Workspace object 
+from azureml.core import Workspace
+from azureml.core.authentication import InteractiveLoginAuthentication
+
+
+def load_configs():
+    with open("config.yml", 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+    return(cfg)
+
+def get_workspace(cfg):
+    interactive_auth = InteractiveLoginAuthentication(tenant_id=cfg['tenant_id'])
+
+    ws = Workspace(subscription_id=cfg['subscription_id'],
+               resource_group=cfg['resource_group'],
+               workspace_name=cfg['workspace_name'],
+               auth=interactive_auth)
+
+    return ws
+
+# load configuraitons
+cfg = load_configs()['azureml']
+
 # may not need Docker Folder nw
 from azureml.core.runconfig import AzureContainerRegistry, DockerEnvironment, EnvironmentDefinition, PythonEnvironment
 registry = AzureContainerRegistry()
-registry.address = 'skywalker61cafcd0.azurecr.io'
-registry.username = 'skywalker61cafcd0'
-registry.password = '1UT6Ndugwrk/j0JcFQKg8V5++f7Yl1nV'
+registry.address = cfg['address']
+registry.username = cfg['username']
+registry.password = cfg['password']
 
 docker_config = DockerEnvironment()
 docker_config.enabled = True
-docker_config.base_image = 'zerowaste/base-gpu:0.2.1'
+docker_config.base_image = 'nfl/base-gpu:0.2.1'
 docker_config.base_image_registry = registry
 docker_config.gpu_support = True
 
